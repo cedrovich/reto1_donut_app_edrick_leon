b36752a50f74b8b4e26aa0bbaa8cf3c3c4c36cff
Cedrovich <ledrickon22@gmail.com>
2024-09-02 14:04:24 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_edrick_leon
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_edrick_leon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_edrick_leon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
